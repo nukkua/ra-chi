fbc61c0b8c30fac055269340da0ea8ec0ab51806
nukkua <marco.salazar.2014.com@gmail.com>
2023-11-12 18:20:06 -0400

    finished jwt

--- internal/app/database/database.go
+++ internal/app/database/database.go
@@ -8,11 +8,20 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/driver/mysql"
 	"github.com/nukkua/ra-chi/internal/app/models"
-	
 )
 
 
 func SetupDatabase () * gorm.DB {
+	
+	
+	requiredEnvVars := []string{"DB_USER","DB_PASSWORD", "DB_HOST", "DB_NAME"}
+
+	for _, envVar := range requiredEnvVars{
+		if os.Getenv(envVar) == ""{
+			log.Fatalf("This env variable is required but not defined: %s", envVar);
+		}
+	}
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 	os.Getenv("DB_USER"),
 	os.Getenv("DB_PASSWORD"),
