f38a3dcb2ce4a71bd18ea652337d568799f6a6aa
nukkua <marco.salazar.2014.com@gmail.com>
2023-11-13 23:44:40 -0400

    CORS added

--- go.mod
+++ go.mod
@@ -4,11 +4,15 @@ go 1.18
 
 require (
 	github.com/go-chi/chi/v5 v5.0.10 // direct
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // direct
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/crypto v0.15.0 // direct
 	gorm.io/driver/mysql v1.5.2 // direct
 	gorm.io/gorm v1.25.5 // direct
+)
+
+require (
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 )
--- internal/app/router/router.go
+++ internal/app/router/router.go
@@ -1,32 +1,44 @@
-package router;
+package router
 
 import (
-	"github.com/nukkua/ra-chi/internal/app/handlers"
-	"github.com/nukkua/ra-chi/internal/app/database"
-	
-
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/nukkua/ra-chi/internal/app/database"
+	"github.com/nukkua/ra-chi/internal/app/handlers"
 	"github.com/nukkua/ra-chi/internal/app/middlewares"
 )
 
-func SetupRouter () *chi.Mux{
-	db:= database.SetupDatabase();
-	
-	r:= chi.NewRouter();
-	r.Use(middleware.Logger);
+func SetupRouter() *chi.Mux {
+	db := database.SetupDatabase()
+
+	r := chi.NewRouter()
+
+	// Configura el middleware CORS aquí
+	corsMiddleware := cors.New(cors.Options{
+		// Define tus opciones de CORS aquí
+		AllowedOrigins:   []string{"https://tufrontend.com"}, // o []string{"*"} para desarrollo
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+
+	// Usar el middleware CORS en todas las rutas
+	r.Use(corsMiddleware.Handler)
+
+	// Otros middlewares y configuraciones
+	r.Use(middleware.Logger)
+
+	// Tus rutas
 	r.Post("/register", handlers.CreateUser(db))
 	r.Post("/login", handlers.LoginUser(db))
 
-	r.Group(func(r chi.Router){
+	r.Group(func(r chi.Router) {
 		r.Use(middlewares.JwtAuthentication)
 		r.Get("/users", handlers.GetUsers(db))
 	})
 
-
-
-
-	return r;
-
+	return r
 }
-
