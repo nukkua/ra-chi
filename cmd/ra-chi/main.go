03a2d70a062729b821367688b2e46ef9d1348c1c
nukkua <marco.salazar.2014.com@gmail.com>
2023-11-13 22:26:02 -0400

    ania

--- cmd/ra-chi/main.go
+++ cmd/ra-chi/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/nukkua/ra-chi/internal/app/router"
 )
@@ -11,7 +12,15 @@ import (
 func main (){
 
 	r:= router.SetupRouter();
+	
+	port:= os.Getenv("PORT");
 	fmt.Println("Initializing server");
-	fmt.Println("Serving at port :8080");
-	log.Fatal(http.ListenAndServe(":8080", r));
+	fmt.Println("Serving at port: "+port);
+
+	if port == ""{
+		port = "3000"
+	}
+
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:" + port, r));
 }
